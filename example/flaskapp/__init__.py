84a7e67e60340ff8bf53ff97928c8ab975b38bb1
ArtyomKozyrev8 <kozirev8@gmail.com>
2020-09-10 21:45:21 +0300

    added endpoint function to demonstrate upd paged table

--- example/flaskapp/__init__.py
+++ example/flaskapp/__init__.py
@@ -41,4 +41,28 @@ def create_app():
             table_rows.append(row_items)
         return jsonify(table_rows)
 
+    @app.route("/paged_upd/<int:rows>/<int:items>", methods=["GET"])
+    def generate_array_for_paginated_table_upd(rows=0, items=0):
+        """
+        Provide data for tables
+        :param rows: number of rows in the table
+        :param items: number of columns in the table
+        :return: list of lists
+        """
+        table_rows = []
+        for i in range(rows):
+            row_items = []
+            if i // 4 == 0:
+                row_items = [999] * items
+            elif i // 7 == 0:
+                row_items = [1998] * items
+            else:
+                for j in range(items):
+                    if j == 0:
+                        row_items.append(j + i)
+                    else:
+                        row_items.append(randint(0, 200))
+            table_rows.append(row_items)
+        return jsonify(table_rows)
+
     return app
